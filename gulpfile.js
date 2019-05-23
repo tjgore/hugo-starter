03eb94fde886ef546e54434b406ee0cbfc383bdc
TJ Gore <gorejrtyndale@gmail.com>
2019-05-22 21:20:41 -0400

    Update to use hugo pipes

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,21 @@
 const gulp = require('gulp')
-const sass = require('gulp-sass')
-const maps = require('gulp-sourcemaps')
 const removeEmptyLines = require('gulp-remove-empty-lines')
 const prettyHtml = require('gulp-pretty-html')
-
+const purgecss = require('gulp-purgecss')
 
 const publicHTML = './public/**/**/*.html'
 const publicDest = './public/'
+const publicCSSDest = './public/css/'
 
-// compile bootstrap scss to css
-const bootstrapSass = () => {
-  return gulp.src(['resources/scss/bootstrap.scss'])
-  .pipe(maps.init())
-  .pipe(sass(
-  {
-    errLogToConsole: true,
-    outputStyle: 'compressed'
-  }))
-  .pipe(maps.write('./maps'))
-  .pipe(gulp.dest('static/css/'))
+// remove unused css
+const removeUnusedCss = () => {
+  return gulp.src('./public/css/*.css')
+  .pipe(
+    purgecss({
+      content: [publicHTML]
+    })
+  )
+  .pipe(gulp.dest(publicCSSDest))
 }
 
 // remove etra white space in html files
@@ -31,17 +28,12 @@ const removeWhiteSpace = () => {
 // prettify html nicely
 const prettify = () => {
   return gulp.src(publicHTML)
-      .pipe(prettyHtml({
-          indent_size: 2,
-          indent_char: ' ',
-          unformatted: ['code', 'pre', 'em', 'strong', 'span', 'i', 'b', 'br'],
-          extra_liners: ['head', 'nav', 'footer', 'section']
-      }))
-      .pipe(gulp.dest(publicDest))
+  .pipe(prettyHtml({
+      indent_size: 2,
+      indent_char: ' ',
+      unformatted: ['code', 'pre', 'em', 'strong', 'span', 'i', 'b', 'br'],
+  }))
+  .pipe(gulp.dest(publicDest))
 }
 
-gulp.task('build', gulp.series(bootstrapSass, removeWhiteSpace, prettify))
-
-gulp.task('watch', function() {
-  gulp.watch('resources/scss/*.scss', gulp.series(gulp.parallel(bootstrapSass)))
-})
+gulp.task('build', gulp.series(removeWhiteSpace, prettify, removeUnusedCss))
